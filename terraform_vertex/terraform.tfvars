dd56e02cd2b9bcb71c61d659c06399fa91b46d5e
PC <aluisaybar@gmail.com>
2025-02-25 20:55:43 -0500

    test cd v5

---
+++ terraform_vertex/terraform.tfvars
@@ -0,0 +1,5 @@
+project_id = "trim-odyssey-390415"
+dataset_id = "github3"
+table_id = "census_by_age"
+region     = "US"
+routine_id = "cencus_filter_by_age"
---
+++ terraform_vertex/variables.tf
@@ -0,0 +1,24 @@
+variable "project_id" {
+  description = ""
+  type = string
+}
+
+variable "dataset_id" {
+  description = ""
+  type = string
+}
+
+variable "table_id" {
+  description = ""
+  type = string
+}
+
+variable "region" {
+  description = ""
+  type = string
+}
+
+variable "routine_id" {
+  description = ""
+  type = string
+}
